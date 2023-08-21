aec4a3dde8f2455b31d09e84c94ffeadf0a8edbd
Saul Ceja <saulceja08@gmail.com>
2023-08-20 23:21:00 -0700

    Complete Lab, adjusted boolean value to be an integer to accomodate sql data type

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'reserved', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8, 'M', 'black', 'playful', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 14, 'M', 'white', 'impulsive', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 21, 'M', 'brown', 'calm', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'black', 'socialble', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 18, 'F', 'brown', 'anxious', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 25, 'M', 'black', 'playful', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'F', 'white', 'playful', TRUE);
-
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'reserved', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8, 'M', 'black', 'playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 14, 'M', 'white', 'impulsive', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 21, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'black', 'socialble', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 18, 'F', 'brown', 'anxious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 25, 'M', 'black', 'playful', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'F', 'white', 'playful', 1);
