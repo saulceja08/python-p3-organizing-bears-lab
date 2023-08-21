2648069c78c6fad6817fa5562be0d8ba80bc561c
Saul Ceja <saulceja08@gmail.com>
2023-08-20 22:03:14 -0700

    Complete Lab

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -8,6 +8,7 @@ select_all_bears_names_and_orders_in_alphabetical_order = """
     ORDER BY name ASC;
 """
 
+
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT name, age FROM bears
     WHERE alive = TRUE
